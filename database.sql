470b06d675800bf5ab376f68d7c9a6450e400d8e
Nate Treimer <natetreimer@gmail.com>
2021-03-21 17:22:55 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -4,5 +4,13 @@ CREATE TABLE "checklist" (
     "complete" BOOLEAN
 );
 
-INSERT INTO "checklist" (task, complete) VALUES ('Walk the dog', false);
-SELECT * from "checklist";
+INSERT INTO "checklist" (task, complete) 
+VALUES
+('Take trash out', true),
+('Rake the leaves', false),
+('Delete e-mails', true),
+('Walk the dog', false),
+('Do PRIME homework', true);
+
+SELECT * from "checklist"
+ORDER BY id;
